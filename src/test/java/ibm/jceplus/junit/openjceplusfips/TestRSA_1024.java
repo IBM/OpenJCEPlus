ccdf7b6658bf5cbeefcb4dc4fcef0a10c8425b91
Jason Katonica <katonica@us.ibm.com>
2024-04-22 09:36:52 -0400

    Revert "Remove RSA Cipher from OpenJCEPlusFIPS"
    There is value in allowing a FIPS module call a non FIPS algoriothm
    with the module so RSA encryption and decryption will be added again
    to the FIPS provider.
    
    This reverts commit 1c30d2888570daa95f214003aa5f8b1db2b4d954.
    
    While reverting this commit the TestRSA_2048 test was being run for the
    first time in the openjceplusfips multithread test suite. An adjustment
    was needed to make it pass by moving the overrided methods for
    isAlgorithmValidButUnsupported and isPaddingValidButUnsupported into the
    child BaseTestRSA class.
    
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

--- src/test/java/ibm/jceplus/junit/openjceplusfips/TestRSA_1024.java
+++ src/test/java/ibm/jceplus/junit/openjceplusfips/TestRSA_1024.java
@@ -6,12 +6,12 @@
  * in the file LICENSE in the source distribution.
  */
 
-package ibm.jceplus.junit.openjceplus;
+package ibm.jceplus.junit.openjceplusfips;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class TestRSA extends ibm.jceplus.junit.base.BaseTestRSA {
+public class TestRSA_1024 extends TestRSA {
 
     //--------------------------------------------------------------------------
     //
@@ -23,15 +23,13 @@ public class TestRSA extends ibm.jceplus.junit.base.BaseTestRSA {
     //--------------------------------------------------------------------------
     //
     //
-    public TestRSA() {
-        super(Utils.TEST_SUITE_PROVIDER_NAME);
-    }
+    private static final int KEY_SIZE = 1024;
 
     //--------------------------------------------------------------------------
     //
     //
-    public TestRSA(int keySize) throws Exception {
-        super(Utils.TEST_SUITE_PROVIDER_NAME, keySize);
+    public TestRSA_1024() throws Exception {
+        super(KEY_SIZE);
     }
 
     //--------------------------------------------------------------------------
@@ -45,7 +43,7 @@ public class TestRSA extends ibm.jceplus.junit.base.BaseTestRSA {
     //
     //
     public static Test suite() {
-        TestSuite suite = new TestSuite(TestRSA.class);
+        TestSuite suite = new TestSuite(TestRSA_1024.class);
         return suite;
     }
 }
