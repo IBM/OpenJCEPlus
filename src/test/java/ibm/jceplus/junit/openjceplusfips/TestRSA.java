b231e9c2e39f732b16e59a3b96f4125bb43a0d23
Jason Katonica <katonica@us.ibm.com>
2024-04-22 08:57:52 -0400

    Revert "Remove RSA Cipher from OpenJCEPlusFIPS"
    There is value in allowing a FIPS module call a non FIPS algoriothm
    with the module so RSA encryption and decryption will be added again
    to the FIPS provider.
    
    This reverts commit 1e28d11e214a9ec54fb7fd466aa31fc8a6c19d18.
    
    While reverting this commit the TestRSA_2048 test was being run for the
    first time in the openjceplusfips multithread test suite. An adjustment
    was needed to make it pass by moving the overrided methods for
    isAlgorithmValidButUnsupported and isPaddingValidButUnsupported into the
    child BaseTestRSA class.
    
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

--- src/test/java/ibm/jceplus/junit/openjceplusfips/TestRSA.java
+++ src/test/java/ibm/jceplus/junit/openjceplusfips/TestRSA.java
@@ -8,17 +8,11 @@
 
 package ibm.jceplus.junit.openjceplusfips;
 
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.Cipher;
-
-import ibm.jceplus.junit.base.BaseTest;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class TestRSA extends BaseTest {
+public class TestRSA extends ibm.jceplus.junit.base.BaseTestRSA {
 
-    protected int specifiedKeySize = 0;
     //--------------------------------------------------------------------------
     //
     //
@@ -37,8 +31,7 @@ public class TestRSA extends BaseTest {
     //
     //
     public TestRSA(int keySize) throws Exception {
-        super(Utils.TEST_SUITE_PROVIDER_NAME);
-        this.specifiedKeySize = keySize;
+        super(Utils.TEST_SUITE_PROVIDER_NAME, keySize);
     }
 
     //--------------------------------------------------------------------------
@@ -54,18 +47,6 @@ public class TestRSA extends BaseTest {
     public static Test suite() {
         TestSuite suite = new TestSuite(TestRSA.class);
         return suite;
-    }
-
-        // --------------------------------------------------------------------------
-    // This method is to check whether OpenJCEPlusFIPS can throw exception with RSA cipher 
-    //
-    public static void testLoadRSACipher() throws Exception{
-        try {
-            Cipher.getInstance("RSA", Utils.TEST_SUITE_PROVIDER_NAME);
-        } catch (NoSuchAlgorithmException nsae) {
-            assertEquals("No such algorithm: RSA", nsae.getMessage());
-            return;
-        }
     }
 }
 
