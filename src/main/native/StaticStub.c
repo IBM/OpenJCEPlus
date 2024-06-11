3c44149386d98d4834da3409744443918d31239a
John Peck <140550562+johnpeck-us-ibm@users.noreply.github.com>
2024-06-11 09:14:53 -0500

    Update initialize function name (#78)
    Updates the initialize function to include package name to be unique. Other functions look to be okay and not likely for a collision.
    
    Tested winx64
    
    Fixes: https://github.com/IBM/OpenJCEPlus/issues/76
    
    Signed-off-by: John Peck <johnpeck@us.ibm.com>

--- src/main/native/StaticStub.c
+++ src/main/native/StaticStub.c
@@ -1,5 +1,5 @@
 /*
- * Copyright IBM Corp. 2023
+ * Copyright IBM Corp. 2023, 2024
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -33,7 +33,7 @@ JNIEXPORT jlong JNICALL Java_com_ibm_crypto_plus_provider_ock_NativeInterface_in
     int retcode = ICC_OK;
     ICC_STATUS status;
 
-    initialize();
+    com_ibm_crypto_plus_provider_initialize();
 
     if( debug ) {
       gslogFunctionEntry(functionName);
