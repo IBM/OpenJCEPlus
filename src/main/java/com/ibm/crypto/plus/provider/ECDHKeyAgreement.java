ce2643e8644c19c6bc2dcb0ec8601cec13f5b7ce
JinhangZhang <Jinhang.Zhang@ibm.com>
2024-08-27 22:46:49 -0400

    Check the scalar value of an ECPrivateKey
    OpenJCEPlus/OpenJCEPlusFIPS providers accepts an invalid
    EC private key at the moment. Need to add a check to ensure
    the EC private key is valid during the initialization procedure
    in key agreement and ecdsa signature.
    
    Signed-off-by: JinhangZhang <Jinhang.Zhang@ibm.com>

--- src/main/java/com/ibm/crypto/plus/provider/ECDHKeyAgreement.java
+++ src/main/java/com/ibm/crypto/plus/provider/ECDHKeyAgreement.java
@@ -312,7 +312,7 @@ public final class ECDHKeyAgreement extends KeyAgreementSpi { // implements
             throw new InvalidKeyException("Key is not an ECPrivateKey");
         }
         java.security.interfaces.ECPrivateKey ecPrivKey = (java.security.interfaces.ECPrivateKey) key;
-
+        sun.security.util.ECUtil.checkPrivateKey(ecPrivKey);
         // check if private key parameters are compatible with
         // initialized ones
         if (params != null) {
