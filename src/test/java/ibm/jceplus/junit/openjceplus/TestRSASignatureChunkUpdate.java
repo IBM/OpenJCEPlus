096f776b4f61ada0b5768caf9946d92e4ac97005
Jason Katonica <122893202+jasonkatonica@users.noreply.github.com>
2024-09-16 17:12:24 -0400

    Refactor some signature related tests for Junit5 (#165)
    This update migrates a portion of the tests associated with
    signatures to Junit5 format. Updates include:
    - A new BaseTestJunit5 class is provided that tests can extend in this
    update and in the future.
    - Main methods in individual tests were present inconsistently through
    the test code and have been removed given that individual tests can be
    executed directly via mvn surefire or other Junit5 runners.
    - Constructors for various tests have also been removed since they are
    not in use.
    - The `TestTLS` was found to impact other tests depending on the order
    tests are run. If the `OpenJCEPlus` provider is present when running
    `OpenJCEPlusFIPS` related tests then it is removed, then restored
    after the test is run. A similar pattern is followed when executing a
    similar test with `OpenJCEPlus`.
    - A few methods were found to be private and should not be declared as
    public.
    - Most tests are currently written to expect a single instance of a
    given test for each @Test method specified. By default this is not the
    behavior in Junit5. This update tags various tests with
    @TestInstance of Lifecycle.PER_CLASS. This tells JUnit to create only
    one instance of the test class and then reuse it between tests tagged
    with @Test.
    - `static` blocks are often replaced with `@BeforeAll` syntax executed
    when a test is constructed.
    - The higher level test suite classes were modified to also use junit5
    syntax ( `@SelectClasses` and `@Suite`).
    - Empty comment blocks have been removed as they have no value.
    - Mulithreaded test case drivers have been modified to use new Junit5
    launchers to launch test classes contained in
    `org.junit.platform.launcher package`.
    
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

--- src/test/java/ibm/jceplus/junit/openjceplus/TestRSASignatureChunkUpdate.java
+++ src/test/java/ibm/jceplus/junit/openjceplus/TestRSASignatureChunkUpdate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright IBM Corp. 2023
+ * Copyright IBM Corp. 2023, 2024
  *
  * Licensed under the Apache License 2.0 (the "License").  You may not use
  * this file except in compliance with the License.  You can obtain a copy
@@ -7,28 +7,17 @@
  */
 
 package ibm.jceplus.junit.openjceplus;
+import ibm.jceplus.junit.base.BaseTestRSASignatureChunkUpdate;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+@TestInstance(Lifecycle.PER_CLASS)
+public class TestRSASignatureChunkUpdate extends BaseTestRSASignatureChunkUpdate {
 
-public class TestRSASignatureChunkUpdate
-        extends ibm.jceplus.junit.base.BaseTestRSASignatureChunkUpdate {
-
-    static {
+    @BeforeAll
+    public void beforeAll() {
         Utils.loadProviderTestSuite();
-    }
-
-    public TestRSASignatureChunkUpdate() {
-        super(Utils.TEST_SUITE_PROVIDER_NAME);
-    }
-
-    public static void main(String[] args) throws Exception {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static Test suite() {
-        TestSuite suite = new TestSuite(
-                ibm.jceplus.junit.openjceplus.TestRSASignatureChunkUpdate.class);
-        return suite;
+        setProviderName(Utils.TEST_SUITE_PROVIDER_NAME);
     }
 }
