25d34b8e0ba551bba1d61da23843ff0993ed515b
Jason Katonica <katonica@us.ibm.com>
2024-10-14 12:49:47 -0400

    Use correct output size for AES on z platform
    When the fast z command is available the AES cipher does not return the
    correct output size on platform z.
    
    Platform z uses a different buffer and output size calculation. The
    method `engineGetOutputSize` when running on platform z should return
    and take this into account.
    
    A new test was added to exercise byte buffers and various combinations
    of encryption, decryption, and orders of doFinal and update operations
    which can be used to recreate the problem.
    
    Closes #240
    
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

---
+++ src/test/java/ibm/jceplus/junit/openjceplusfips/TestResetByteBuffer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright IBM Corp. 2024
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution.
+ */
+
+package ibm.jceplus.junit.openjceplusfips;
+
+import ibm.jceplus.junit.base.BaseTestResetByteBuffer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class TestResetByteBuffer extends BaseTestResetByteBuffer {
+
+    @BeforeAll
+    public void beforeAll() {
+        Utils.loadProviderTestSuite();
+        setProviderName(Utils.TEST_SUITE_PROVIDER_NAME);
+    }
+}
