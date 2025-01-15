f0e14e683ffbef91707d7f4f9107eaa83ce9fd2b
Jason Katonica <katonica@us.ibm.com>
2025-01-15 11:18:00 -0500

    Add support for PBKDF2 algorithms
    The following algorithms will be added to the OpenJCEPlusFIPS provider:
    - PBKDF2WithHmacSHA224
    - PBKDF2WithHmacSHA256
    - PBKDF2WithHmacSHA384
    - PBKDF2WithHmacSHA512
    
    The following algorithms will be added to the OpenJCEPlus provider:
    - PBKDF2WithHmacSHA1
    - PBKDF2WithHmacSHA224
    - PBKDF2WithHmacSHA256
    - PBKDF2WithHmacSHA384
    - PBKDF2WithHmacSHA512
    
    Updates required include:
    - Adding a new PBKDF2Core class based on OpenJDK.
    - Adding a new PBKDF2KeyImpl class based on OpenJDK yet modified to
    make use of the OCKC library to perform PBKDF2 key derivations.
    - New JNI code to call a new native method to derive a key using PBKDF2
    from a given password, salt, iteration count, and desired key length.
    - Updates in makefiles to allow the new PBKDF.c file to be built into
    the JNI dll.
    - A new set of interoperability test was created to enforce that we get the same
    results from SunJCE, OpenJCEPlus, OpenJCEPlusFIPS for the KeyFactory
    methods `generateSecret`, `translateKey`, and `getKeySpec`.
    - A new test was introduce to drive various KAT and error paths.
    
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

---
+++ src/test/java/ibm/jceplus/junit/openjceplusfips/TestPBKDF2Interop.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright IBM Corp. 2023, 2025
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution.
+ */
+
+package ibm.jceplus.junit.openjceplusfips;
+
+import ibm.jceplus.junit.base.BaseTestPBKDF2Interop;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class TestPBKDF2Interop extends BaseTestPBKDF2Interop {
+
+    @BeforeAll
+    public void beforeAll() {
+        Utils.loadProviderTestSuite();
+        setProviderName(Utils.TEST_SUITE_PROVIDER_NAME);
+        setInteropProviderName(Utils.PROVIDER_SunJCE);
+    }
+}
