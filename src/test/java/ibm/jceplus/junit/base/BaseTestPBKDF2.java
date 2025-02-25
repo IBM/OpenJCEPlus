7e93622299ea8fc80d440362f578a4d9f516f4c8
Jason Katonica <122893202+jasonkatonica@users.noreply.github.com>
2025-02-25 15:47:13 -0500

    Add support for PBKDF2 algorithms (#471)
    The following algorithms will be added to the OpenJCEPlusFIPS provider:
    - PBKDF2WithHmacSHA224
    - PBKDF2WithHmacSHA256
    - PBKDF2WithHmacSHA384
    - PBKDF2WithHmacSHA512
    
    The following algorithms will be added to the OpenJCEPlus provider:
    - PBKDF2WithHmacSHA1
    - PBKDF2WithHmacSHA224
    - PBKDF2WithHmacSHA256
    - PBKDF2WithHmacSHA384
    - PBKDF2WithHmacSHA512
    
    Updates required include:
    - Adding a new PBKDF2Core class based on OpenJDK.
    - Adding a new PBKDF2KeyImpl class based on OpenJDK yet modified to
    make use of the OCKC library to perform PBKDF2 key derivations.
    - New JNI code to call a new native method to derive a key using PBKDF2
    from a given password, salt, iteration count, and desired key length.
    - Updates in makefiles to allow the new PBKDF.c file to be built into
    the JNI dll.
    - A new set of interoperability test was created to enforce that we get the same
    results from SunJCE, OpenJCEPlus, OpenJCEPlusFIPS for the KeyFactory
    methods `generateSecret`, `translateKey`, and `getKeySpec`.
    - A new test was introduce to drive various KAT and error paths.
    
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

---
+++ src/test/java/ibm/jceplus/junit/base/BaseTestPBKDF2.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright IBM Corp. 2025
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution.
+ */
+package ibm.jceplus.junit.base;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests associated with PBKDF2 algorithms.
+ * 
+ */
+public class BaseTestPBKDF2 extends BaseTestJunit5Interop {
+    static List<String> allowableFIPSAlgorithms = new ArrayList<String>(){{
+            add("PBKDF2WithHmacSHA224");
+            add("PBKDF2WithHmacSHA256");
+            add("PBKDF2WithHmacSHA384");
+            add("PBKDF2WithHmacSHA512");
+        }};
+
+    /**
+    * Official test vector from RFC 7914.
+    * 
+    * 11.  Test Vectors for PBKDF2 with HMAC-SHA-256
+    * 
+    * Below is a sequence of octets that illustrate input and output values
+    * for PBKDF2-HMAC-SHA-256.  The octets are hex encoded and whitespace
+    * is inserted for readability.  The test vectors below can be used to
+    * verify the PBKDF2-HMAC-SHA-256 [RFC2898] function.  The password and
+    * salt strings are passed as sequences of ASCII [RFC20] octets.
+    *
+    * PBKDF2-HMAC-SHA-256 (P="passwd", S="salt",
+    *                     c=1, dkLen=64) =
+    * 55 ac 04 6e 56 e3 08 9f ec 16 91 c2 25 44 b6 05
+    * f9 41 85 21 6d de 04 65 e6 8b 9d 57 c2 0d ac bc
+    * 49 ca 9c cc f1 79 b6 45 99 16 64 b3 9d 77 ef 31
+    * 7c 71 b8 45 b1 e3 0b d5 09 11 20 41 d3 a1 97 83
+    *
+    * PBKDF2-HMAC-SHA-256 (P="Password", S="NaCl",
+    *                      c=80000, dkLen=64) =
+    * 4d dc d8 f6 0b 98 be 21 83 0c ee 5e f2 27 01 f9
+    * 64 1a 44 18 d0 4c 04 14 ae ff 08 87 6b 34 ab 56
+    * a1 d4 25 a1 22 58 33 54 9a db 84 1b 51 c9 b3 17
+    * 6a 27 2b de bb a1 d0 78 47 8f 62 b3 97 f3 3c 8d
+    *
+    * @param algorithm
+    * @throws Exception
+    */
+    @Test
+    public void testPBKDF2KAT() throws Exception {
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256",
+                        this.getProviderName());
+        PBEKeySpec pbeks = new PBEKeySpec("passwd".toCharArray(), "salt".getBytes("ASCII"),
+                        1, 512);
+        SecretKey sk = skf.generateSecret(pbeks);
+        String hexKeyValue = BaseUtils.bytesToHex(sk.getEncoded());
+        assertEquals("55ac046e56e3089fec1691c22544b605" +
+                     "f94185216dde0465e68b9d57c20dacbc" +
+                     "49ca9cccf179b645991664b39d77ef31" +
+                     "7c71b845b1e30bd509112041d3a19783", hexKeyValue,
+                        "RFC Known answer test failed for PBKDF2WithHmacSHA256.");
+        pbeks = new PBEKeySpec("Password".toCharArray(), "NaCl".getBytes("ASCII"), 80000,
+                        512);
+        sk = skf.generateSecret(pbeks);
+        hexKeyValue = BaseUtils.bytesToHex(sk.getEncoded());
+        assertEquals("4ddcd8f60b98be21830cee5ef22701f9" +
+                     "641a4418d04c0414aeff08876b34ab56" +
+                     "a1d425a1225833549adb841b51c9b317" +
+                     "6a272bdebba1d078478f62b397f33c8d", hexKeyValue,
+                        "RFC Known answer test failed for PBKDF2WithHmacSHA256.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"PBKDF2WithHmacSHA1", "PBKDF2WithHmacSHA224", "PBKDF2WithHmacSHA256",
+        "PBKDF2WithHmacSHA384", "PBKDF2WithHmacSHA512"})
+    public void testAlgorithmExistence(String algorithm) throws Exception {
+        try {
+            SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+        } catch (NoSuchAlgorithmException e) {
+            // The FIPS provider does not allow for PBKDF2WithHmacSHA1 and PBKDF2WithHmacSHA224.
+            if ((!isSupportedByOpenJCEPlusFIPS(algorithm)) && this.getProviderName().equalsIgnoreCase("OpenJCEPlusFIPS")) {
+                return;
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"PBKDF2WithHmacSHA224", "PBKDF2WithHmacSHA256",
+        "PBKDF2WithHmacSHA384", "PBKDF2WithHmacSHA512"})
+    public void testSmallSalt(String algorithm) throws Exception {
+        
+        PBEKeySpec pbeks = new PBEKeySpec("ABCDEFGHIJ".toCharArray(), "SmallSalt".getBytes(), 10000, 512);
+        if (this.getProviderName().equalsIgnoreCase("OpenJCEPlusFIPS")) {
+            try {
+                SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+                skf.generateSecret(pbeks);
+            } catch(InvalidKeySpecException e) {
+                assertEquals("Salt must be 128 bits or higher when using the OpenJCEPlusFIPS provider.", e.getMessage());
+            }
+        } else {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+            skf.generateSecret(pbeks);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"PBKDF2WithHmacSHA224", "PBKDF2WithHmacSHA256",
+        "PBKDF2WithHmacSHA384", "PBKDF2WithHmacSHA512"})
+    public void testSmallIterationCount(String algorithm) throws Exception {
+        PBEKeySpec pbeks = new PBEKeySpec("ABCDEFGHIJ".toCharArray(), new byte[32], 999, 512);
+        if (this.getProviderName().equalsIgnoreCase("OpenJCEPlusFIPS")) {
+            try {
+                SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+                skf.generateSecret(pbeks);
+            } catch(InvalidKeySpecException e) {
+                assertEquals("Iteration count must be 1000 or higher when using the OpenJCEPlusFIPS provider.", e.getMessage());
+            }
+        } else {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+            skf.generateSecret(pbeks);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"PBKDF2WithHmacSHA224", "PBKDF2WithHmacSHA256",
+        "PBKDF2WithHmacSHA384", "PBKDF2WithHmacSHA512"})
+    public void testSmallKeyLength(String algorithm) throws Exception {
+        PBEKeySpec pbeks = new PBEKeySpec("ABCDEFGHIJ".toCharArray(), new byte[32], 8000, 111);
+        if (this.getProviderName().equalsIgnoreCase("OpenJCEPlusFIPS")) {
+            try {
+                SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+                skf.generateSecret(pbeks);
+            } catch(InvalidKeySpecException e) {
+                assertEquals("Key length must be 112 bits or higher when using the OpenJCEPlusFIPS provider.", e.getMessage());
+            }
+        } else {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+            skf.generateSecret(pbeks);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({"PBKDF2WithHmacSHA224", "PBKDF2WithHmacSHA256",
+        "PBKDF2WithHmacSHA384", "PBKDF2WithHmacSHA512"})
+    public void testShortPassword(String algorithm) throws Exception {
+        PBEKeySpec pbeks = new PBEKeySpec("ABCDEFGHI".toCharArray(), new byte[32], 1000, 112);
+        if (this.getProviderName().equalsIgnoreCase("OpenJCEPlusFIPS")) {
+            try {
+                SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+                skf.generateSecret(pbeks);
+            } catch(InvalidKeySpecException e) {
+                assertEquals("Password must be 10 characters or higher when using the OpenJCEPlusFIPS provider.", e.getMessage());
+            }
+        } else {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+            skf.generateSecret(pbeks);
+        }
+    }
+
+    /**
+     * Method to help determine if the OpenJCEPlusFIPS provider supports an algorithm.
+     * 
+     * @param algorithm
+     * @return
+     */
+    private boolean isSupportedByOpenJCEPlusFIPS(String algorithm) {
+        for (String allowed : allowableFIPSAlgorithms) {
+            if (allowed.equalsIgnoreCase(algorithm)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
