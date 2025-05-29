5f5bbf7370c5fc85f0de38746e40ddf7187306c1
John Peck <140550562+johnpeck-us-ibm@users.noreply.github.com>
2025-05-29 10:58:38 -0500

    Clang format my .c files
    Signed-off-by: John Peck <140550562+johnpeck-us-ibm@users.noreply.github.com>

--- src/main/native/KEM.c
+++ src/main/native/KEM.c
@@ -23,85 +23,86 @@
  * Method:    KEM_encapsulate
  * Signature: (JJ[B[B)V
  */
-JNIEXPORT void JNICALL Java_com_ibm_crypto_plus_provider_ock_NativeInterface_KEM_1encapsulate
-  (JNIEnv *env, jclass thisObj, jlong ockContextId, jlong ockPKeyId, jbyteArray wrappedKey, jbyteArray randomKey)
-{
-  static const char * functionName = "NativeInterface.KEM_encapsulate";
+JNIEXPORT void JNICALL
+Java_com_ibm_crypto_plus_provider_ock_NativeInterface_KEM_1encapsulate(
+    JNIEnv *env, jclass thisObj, jlong ockContextId, jlong ockPKeyId,
+    jbyteArray wrappedKey, jbyteArray randomKey) {
+    static const char *functionName = "NativeInterface.KEM_encapsulate";
 
-  ICC_CTX *         ockCtx = (ICC_CTX *)((intptr_t) ockContextId);
-  ICC_EVP_PKEY_CTX* evp_pk;
-  ICC_EVP_PKEY*     pa = (ICC_EVP_PKEY *) ((intptr_t) ockPKeyId);
-  jlong             mlkeyId = 0;
-  size_t            wrappedkeylen = 0;
-  size_t            genkeylen = 0;
-  jboolean          isCopy = 0;
-  unsigned char *   wrappedKeyLocal = NULL;
-  unsigned char *   genkeylocal = NULL;
-  unsigned char *   wrappedKeyNative = NULL;
-  unsigned char *   genKeyNative = NULL;
+    ICC_CTX          *ockCtx = (ICC_CTX *)((intptr_t)ockContextId);
+    ICC_EVP_PKEY_CTX *evp_pk;
+    ICC_EVP_PKEY     *pa               = (ICC_EVP_PKEY *)((intptr_t)ockPKeyId);
+    jlong             mlkeyId          = 0;
+    size_t            wrappedkeylen    = 0;
+    size_t            genkeylen        = 0;
+    jboolean          isCopy           = 0;
+    unsigned char    *wrappedKeyLocal  = NULL;
+    unsigned char    *genkeylocal      = NULL;
+    unsigned char    *wrappedKeyNative = NULL;
+    unsigned char    *genKeyNative     = NULL;
 
-  evp_pk = ICC_EVP_PKEY_CTX_new_from_pkey(ockCtx, NULL, pa, NULL);
-  if (!evp_pk) {
-    throwOCKException(env, 0, "ICC_EVP_PKEY_CTX_new_from_pkey failed");
-    return;
-  }
-  
-  int rc = -1;
-
-  rc = ICC_EVP_PKEY_encapsulate_init(ockCtx, NULL, NULL);
-  if (rc != ICC_OSSL_SUCCESS) {
-    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
-    throwOCKException(env, 0, "ICC_EVP_PKEY_encapsulate_init failed");
-    return;
-  }
-
-  rc = ICC_EVP_PKEY_encapsulate(ockCtx, evp_pk, NULL, &wrappedkeylen, NULL, &genkeylen);
-  if (rc != ICC_OSSL_SUCCESS) {
-    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
-    throwOCKException(env, 0,"ICC_EVP_PKEY_encapsulate failed getting lenghts");
-    return;
-  }  
-
-  wrappedKeyLocal = (unsigned char *)malloc(wrappedkeylen);
-  genkeylocal = (unsigned char *)malloc(genkeylen);
-  if (wrappedKeyLocal == NULL || genkeylocal == NULL) {
-    if (wrappedKeyLocal != NULL){
-      free(wrappedKeyLocal);
+    evp_pk = ICC_EVP_PKEY_CTX_new_from_pkey(ockCtx, NULL, pa, NULL);
+    if (!evp_pk) {
+        throwOCKException(env, 0, "ICC_EVP_PKEY_CTX_new_from_pkey failed");
+        return;
     }
-    if (genkeylocal != NULL){
-      free(wrappedKeyLocal);
-    } 
-    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk); 
-    throwOCKException(env, 0, "malloc failed");
-    return;
-  } else {
 
-    rc = ICC_EVP_PKEY_encapsulate(ockCtx, evp_pk, wrappedKeyLocal, &wrappedkeylen, genkeylocal, &genkeylen);
+    int rc = -1;
 
+    rc = ICC_EVP_PKEY_encapsulate_init(ockCtx, NULL, NULL);
     if (rc != ICC_OSSL_SUCCESS) {
-      if (wrappedKeyLocal != NULL){
-        free(wrappedKeyLocal);
-      }
-      if (genkeylocal != NULL){
-        free(wrappedKeyLocal);
-      } 
-      ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
-      throwOCKException(env, 0,"ICC_EVP_PKEY_encapsulate failed");
-      return;
+        ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+        throwOCKException(env, 0, "ICC_EVP_PKEY_encapsulate_init failed");
+        return;
     }
 
-    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+    rc = ICC_EVP_PKEY_encapsulate(ockCtx, evp_pk, NULL, &wrappedkeylen, NULL,
+                                  &genkeylen);
+    if (rc != ICC_OSSL_SUCCESS) {
+        ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+        throwOCKException(env, 0,
+                          "ICC_EVP_PKEY_encapsulate failed getting lenghts");
+        return;
+    }
 
-    jbyte *bytes = (*env)->GetByteArrayElements(env, wrappedKey, NULL);   
-    memcpy(bytes, wrappedKeyLocal, wrappedkeylen);
-    (*env)->ReleaseByteArrayElements(env, wrappedKey, bytes, 0);
+    wrappedKeyLocal = (unsigned char *)malloc(wrappedkeylen);
+    genkeylocal     = (unsigned char *)malloc(genkeylen);
+    if (wrappedKeyLocal == NULL || genkeylocal == NULL) {
+        if (wrappedKeyLocal != NULL) {
+            free(wrappedKeyLocal);
+        }
+        if (genkeylocal != NULL) {
+            free(wrappedKeyLocal);
+        }
+        ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+        throwOCKException(env, 0, "malloc failed");
+        return;
+    } else {
+        rc = ICC_EVP_PKEY_encapsulate(ockCtx, evp_pk, wrappedKeyLocal,
+                                      &wrappedkeylen, genkeylocal, &genkeylen);
 
-    bytes = (*env)->GetByteArrayElements(env, randomKey, NULL);
-    memcpy(bytes, genkeylocal, genkeylen);
-    (*env)->ReleaseByteArrayElements(env, randomKey, bytes, 0);
+        if (rc != ICC_OSSL_SUCCESS) {
+            if (wrappedKeyLocal != NULL) {
+                free(wrappedKeyLocal);
+            }
+            if (genkeylocal != NULL) {
+                free(wrappedKeyLocal);
+            }
+            ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+            throwOCKException(env, 0, "ICC_EVP_PKEY_encapsulate failed");
+            return;
+        }
 
-  }  
+        ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
 
+        jbyte *bytes = (*env)->GetByteArrayElements(env, wrappedKey, NULL);
+        memcpy(bytes, wrappedKeyLocal, wrappedkeylen);
+        (*env)->ReleaseByteArrayElements(env, wrappedKey, bytes, 0);
+
+        bytes = (*env)->GetByteArrayElements(env, randomKey, NULL);
+        memcpy(bytes, genkeylocal, genkeylen);
+        (*env)->ReleaseByteArrayElements(env, randomKey, bytes, 0);
+    }
 }
 
 //============================================================================
@@ -110,98 +111,108 @@ JNIEXPORT void JNICALL Java_com_ibm_crypto_plus_provider_ock_NativeInterface_KEM
  * Method:    MLKEY_decapsulate
  * Signature: (JJ[B)[B
  */
-JNIEXPORT jbyteArray JNICALL Java_com_ibm_crypto_plus_provider_ock_NativeInterface_KEM_1decapsulate
-  (JNIEnv *env, jclass thisObj, jlong ockContextId, jlong ockPKeyId, jbyteArray wrappedKey)
-{
-  static const char * functionName = "NativeInterface.KEM_decapsulate";
+JNIEXPORT jbyteArray JNICALL
+Java_com_ibm_crypto_plus_provider_ock_NativeInterface_KEM_1decapsulate(
+    JNIEnv *env, jclass thisObj, jlong ockContextId, jlong ockPKeyId,
+    jbyteArray wrappedKey) {
+    static const char *functionName = "NativeInterface.KEM_decapsulate";
 
-  ICC_CTX *                ockCtx = (ICC_CTX *)((intptr_t) ockContextId);
-  ICC_EVP_PKEY *           ockPKey = (ICC_EVP_PKEY *) ((intptr_t) ockPKeyId);
-  ICC_EVP_PKEY_CTX*        evp_pk;
-  ICC_EVP_PKEY*            priv = NULL;
-  ICC_PKCS8_PRIV_KEY_INFO* p8 = NULL;
-  int                      rc = -1;
-  jboolean                 isCopy = 0;
-  jbyteArray               randomKey;
-  jbyteArray               retRndKeyBytes = NULL;
-  size_t                   wrappedkeylen = 0;
-  size_t                   genkeylen = 0;
-  unsigned char *          wrappedKeyNative = NULL;
-  unsigned char *          genkeylocal = NULL;
-  unsigned char *          genKeyNative = NULL;
+    ICC_CTX                 *ockCtx  = (ICC_CTX *)((intptr_t)ockContextId);
+    ICC_EVP_PKEY            *ockPKey = (ICC_EVP_PKEY *)((intptr_t)ockPKeyId);
+    ICC_EVP_PKEY_CTX        *evp_pk;
+    ICC_EVP_PKEY            *priv   = NULL;
+    ICC_PKCS8_PRIV_KEY_INFO *p8     = NULL;
+    int                      rc     = -1;
+    jboolean                 isCopy = 0;
+    jbyteArray               randomKey;
+    jbyteArray               retRndKeyBytes   = NULL;
+    size_t                   wrappedkeylen    = 0;
+    size_t                   genkeylen        = 0;
+    unsigned char           *wrappedKeyNative = NULL;
+    unsigned char           *genkeylocal      = NULL;
+    unsigned char           *genKeyNative     = NULL;
 
-  evp_pk = ICC_EVP_PKEY_CTX_new(ockCtx, ockPKey, NULL);
-  if (!evp_pk) {
-    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
-    throwOCKException(env, 0, "ICC_EVP_PKEY_CTX_new_from_pkey failed");
-    return retRndKeyBytes;
-  }
+    evp_pk = ICC_EVP_PKEY_CTX_new(ockCtx, ockPKey, NULL);
+    if (!evp_pk) {
+        ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+        throwOCKException(env, 0, "ICC_EVP_PKEY_CTX_new_from_pkey failed");
+        return retRndKeyBytes;
+    }
 
-  rc = ICC_EVP_PKEY_decapsulate_init(ockCtx, NULL, NULL);
-
-  if (rc != ICC_OSSL_SUCCESS) {
-    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
-    throwOCKException(env, 0, "ICC_EVP_PKEY_decapsulate_init failed");
-    return retRndKeyBytes;
-  }
-  wrappedKeyNative = (unsigned char*)((*env)->GetPrimitiveArrayCritical(env, wrappedKey, &isCopy));
-  
-  if( NULL == wrappedKeyNative ) {
-    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
-    throwOCKException(env, 0, "NULL from GetPrimitiveArrayCritical!");
-    return retRndKeyBytes;
-  }
-
-  wrappedkeylen = (*env)->GetArrayLength(env, wrappedKey);
-
-  rc = ICC_EVP_PKEY_decapsulate(ockCtx, evp_pk, NULL, &genkeylen, NULL, wrappedkeylen);
-
-  if (rc != ICC_OSSL_SUCCESS) {
-    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
-    (*env)->ReleasePrimitiveArrayCritical(env, wrappedKey, wrappedKeyNative, 0);
-    throwOCKException(env, 0, "ICC_EVP_PKEY_deccapsulate to get lenghts failed");
-    return retRndKeyBytes;
-  }  
-
-  genkeylocal = (unsigned char *)malloc(genkeylen);
-  if (genkeylocal == NULL) {
-    throwOCKException(env, 0, "malloc failed");
-  } else {
-    rc = ICC_EVP_PKEY_decapsulate(ockCtx, evp_pk, genkeylocal, &genkeylen, wrappedKeyNative, wrappedkeylen);
+    rc = ICC_EVP_PKEY_decapsulate_init(ockCtx, NULL, NULL);
 
     if (rc != ICC_OSSL_SUCCESS) {
-      ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
-      free (genkeylocal);
-      (*env)->ReleasePrimitiveArrayCritical(env, wrappedKey, wrappedKeyNative, 0);
-      throwOCKException(env, 0, "ICC_EVP_PKEY_deccapsulate failed");
-      return retRndKeyBytes;
+        ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+        throwOCKException(env, 0, "ICC_EVP_PKEY_decapsulate_init failed");
+        return retRndKeyBytes;
+    }
+    wrappedKeyNative = (unsigned char *)((*env)->GetPrimitiveArrayCritical(
+        env, wrappedKey, &isCopy));
+
+    if (NULL == wrappedKeyNative) {
+        ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+        throwOCKException(env, 0, "NULL from GetPrimitiveArrayCritical!");
+        return retRndKeyBytes;
     }
 
-    randomKey = (*env)->NewByteArray(env, genkeylen);
+    wrappedkeylen = (*env)->GetArrayLength(env, wrappedKey);
 
-    if( randomKey == NULL ) {
-      throwOCKException(env, 0, "NewByteArray failed");
+    rc = ICC_EVP_PKEY_decapsulate(ockCtx, evp_pk, NULL, &genkeylen, NULL,
+                                  wrappedkeylen);
+
+    if (rc != ICC_OSSL_SUCCESS) {
+        ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+        (*env)->ReleasePrimitiveArrayCritical(env, wrappedKey, wrappedKeyNative,
+                                              0);
+        throwOCKException(env, 0,
+                          "ICC_EVP_PKEY_deccapsulate to get lenghts failed");
+        return retRndKeyBytes;
+    }
+
+    genkeylocal = (unsigned char *)malloc(genkeylen);
+    if (genkeylocal == NULL) {
+        throwOCKException(env, 0, "malloc failed");
     } else {
-      genKeyNative = (unsigned char*)((*env)->GetPrimitiveArrayCritical(env, randomKey, &isCopy));
+        rc = ICC_EVP_PKEY_decapsulate(ockCtx, evp_pk, genkeylocal, &genkeylen,
+                                      wrappedKeyNative, wrappedkeylen);
 
-      if( genKeyNative == NULL ) {
-        throwOCKException(env, 0, "NULL from GetPrimitiveArrayCritical");
-      } else {
-        memcpy(genKeyNative, genkeylocal, genkeylen);
-        retRndKeyBytes = randomKey;
-      }
+        if (rc != ICC_OSSL_SUCCESS) {
+            ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+            free(genkeylocal);
+            (*env)->ReleasePrimitiveArrayCritical(env, wrappedKey,
+                                                  wrappedKeyNative, 0);
+            throwOCKException(env, 0, "ICC_EVP_PKEY_deccapsulate failed");
+            return retRndKeyBytes;
+        }
+
+        randomKey = (*env)->NewByteArray(env, genkeylen);
+
+        if (randomKey == NULL) {
+            throwOCKException(env, 0, "NewByteArray failed");
+        } else {
+            genKeyNative = (unsigned char *)((*env)->GetPrimitiveArrayCritical(
+                env, randomKey, &isCopy));
+
+            if (genKeyNative == NULL) {
+                throwOCKException(env, 0,
+                                  "NULL from GetPrimitiveArrayCritical");
+            } else {
+                memcpy(genKeyNative, genkeylocal, genkeylen);
+                retRndKeyBytes = randomKey;
+            }
+        }
     }
-  }
 
-  if (genKeyNative != NULL) {
-    (*env)->ReleasePrimitiveArrayCritical(env, randomKey, genKeyNative, 0);
-  }   
+    if (genKeyNative != NULL) {
+        (*env)->ReleasePrimitiveArrayCritical(env, randomKey, genKeyNative, 0);
+    }
 
-  if (wrappedKeyNative != NULL) {
-    (*env)->ReleasePrimitiveArrayCritical(env, wrappedKey, wrappedKeyNative, 0);
-  }  
+    if (wrappedKeyNative != NULL) {
+        (*env)->ReleasePrimitiveArrayCritical(env, wrappedKey, wrappedKeyNative,
+                                              0);
+    }
 
-  ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
+    ICC_EVP_PKEY_CTX_free(ockCtx, evp_pk);
 
-  return retRndKeyBytes;
+    return retRndKeyBytes;
 }
