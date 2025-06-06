2c183dd891dcbf3f4b7a0ca8621bac7e6493bcf4
John Peck <140550562+johnpeck-us-ibm@users.noreply.github.com>
2025-06-06 11:47:51 -0500

    Add code block that was removed becasue of review comment.
    Signed-off-by: John Peck <140550562+johnpeck-us-ibm@users.noreply.github.com>

--- src/main/native/MLKey.c
+++ src/main/native/MLKey.c
@@ -191,7 +191,20 @@ Java_com_ibm_crypto_plus_provider_ock_NativeInterface_MLKEY_1generate(
     /* public */
     const unsigned char *cpp = pubdata;
     len = publen;
-
+    
+    if (!nid) {
+        nid = ICC_OBJ_txt2nid(ockCtx, algoChars);
+        if (!nid) {
+#ifdef DEBUG_PQC_KEY_DETAIL
+            if (debug) {
+                gslogMessage("ICC_OBJ_txt2nid failed- %s", algoChars);
+            }
+#endif
+            throwOCKException(env, 0,
+                              "Key generation failed - ICC_OBJ_txt2nid");
+            return 0;
+        }
+    }
     /* Reconstruct public key from encoding and type */
     npa = ICC_d2i_PublicKey(ockCtx, nid, &npa, &cpp, len);
 
