91803f50794197630d8aa2c4cdb1bf6b753f03a5
Jason Katonica <katonica@us.ibm.com>
2025-06-06 13:41:46 -0400

    Add PBKDF2 multithreaded tests
    Tests for the PBKDF2 algorithm have been found to not be running
    correctly when attempted to be run with multiple threads. This update
    removes some problematic static variables and adds the PBKDF2 related
    tests to the multi threaded test suites for both FIPS and non FIPS
    testing.
    
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

---
+++ src/test/java/ibm/jceplus/junit/openjceplusfips/multithread/TestPBKDF2Interop.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright IBM Corp. 2023, 2025
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms provided by IBM in the LICENSE file that accompanied
+ * this code, including the "Classpath" Exception described therein.
+ */
+
+package ibm.jceplus.junit.openjceplusfips.multithread;
+
+import ibm.jceplus.junit.base.BaseTestPBKDF2Interop;
+import ibm.jceplus.junit.openjceplus.Utils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class TestPBKDF2Interop extends BaseTestPBKDF2Interop {
+
+    @BeforeAll
+    public void beforeAll() {
+        Utils.loadProviderTestSuite();
+        setProviderName(Utils.TEST_SUITE_PROVIDER_NAME);
+        setInteropProviderName(Utils.PROVIDER_SunJCE);
+    }
+}
