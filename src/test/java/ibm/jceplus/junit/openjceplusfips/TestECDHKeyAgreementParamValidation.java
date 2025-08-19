658cc4dbad1859bd5cde59c3a1ecd82593ca4db5
JinhangZhang <Jinhang.Zhang@ibm.com>
2025-08-19 00:37:31 -0400

    Restrict EC curves in computing shared secret
    According to the policy, curve size less than 192 should
    not be allowed in computing shared secret in ECDH
    keyagreement in FIPS 140-3 mode. This PR proposes an option
    to enable/disable this behaviour.
    
    Signed-off-by: JinhangZhang <Jinhang.Zhang@ibm.com>

--- src/test/java/ibm/jceplus/junit/openjceplusfips/TestECDHKeyAgreementParamValidation.java
+++ src/test/java/ibm/jceplus/junit/openjceplusfips/TestECDHKeyAgreementParamValidation.java
@@ -8,9 +8,24 @@
 package ibm.jceplus.junit.openjceplusfips;
 
 import ibm.jceplus.junit.base.BaseTestECDHKeyAgreementParamValidation;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.ECGenParameterSpec;
+
+import javax.crypto.KeyAgreement;
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class TestECDHKeyAgreementParamValidation extends BaseTestECDHKeyAgreementParamValidation {
@@ -20,5 +35,34 @@ public class TestECDHKeyAgreementParamValidation extends BaseTestECDHKeyAgreemen
         Utils.loadProviderTestSuite();
         setProviderName(Utils.TEST_SUITE_PROVIDER_NAME);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"secp112r1", "secp112r2", "secp128r1", "secp128r2", "secp160r1", "secp160r2", "secp160k1", "secp192k1", "secp192r1"})
+    public void testECDHKeyAgreementSharedSecretComputation(String curveName) {
+        try {
+            KeyPair alice = genECKeyPair(curveName);
+            KeyPair bob   = genECKeyPair(curveName);
+
+            byte[] secretAlice = ecdhSharedSecretComputation(alice.getPrivate(), bob.getPublic());
+            byte[] secretBob   = ecdhSharedSecretComputation(bob.getPrivate(), alice.getPublic());
+        } catch (Exception e) {
+            assertTrue(e.getMessage().equals(curveName + " curve is not supported in FIPS for calculating the shared secret"));
+        }
+    }
+
+    private KeyPair genECKeyPair(String curveName) throws Exception {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC", getProviderName());
+        kpg.initialize(new ECGenParameterSpec(curveName));
+        KeyPair kp = kpg.generateKeyPair();
+        return kp;
+    }
+
+    private byte[] ecdhSharedSecretComputation(PrivateKey priv, PublicKey peerPub) throws Exception {
+        KeyAgreement ka = KeyAgreement.getInstance("ECDH", getProviderName());
+        ka.init(priv);
+        ka.doPhase(peerPub, true);
+        return ka.generateSecret();
+    }
+
 
 }
