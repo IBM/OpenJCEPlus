07aaa88b609417af3c9063524f207b157d9809db
Dev Agarwal <devagarwal0508@gmail.com>
2025-09-18 13:37:45 -0400

    Support for PBES2, PBEKeyfactory, PBEKey and PBEParameters
    This update adds support for PBES2, PBEKeyFactory, PBEKey,
    and PBEParameters for PBE algorithms with Hmac and AES for
    OpenJCEPlus provider.
    
    Signed-off-by: Dev Agarwal <dev.agarwal@ibm.com>

---
+++ src/test/java/ibm/jceplus/junit/base/BaseTestPBEKeyFactory.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright IBM Corp. 2025
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms provided by IBM in the LICENSE file that accompanied
+ * this code, including the "Classpath" Exception described therein.
+ */
+
+package ibm.jceplus.junit.base;
+
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class BaseTestPBEKeyFactory extends BaseTestJunit5 {
+
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256", "PBEWithHmacSHA512/224AndAES_128", "PBEWithHmacSHA512/224AndAES_256",
+        "PBEWithHmacSHA512/256AndAES_128", "PBEWithHmacSHA512/256AndAES_256"})
+    public void testValid(String algorithm) throws Exception {
+
+        SecretKeyFactory fac = SecretKeyFactory.getInstance(algorithm, getProviderName());
+
+        char[] pass = new char[] {'p', 'a', 's', 's', 'w', 'o', 'r', 'd'};
+        PBEKeySpec spec = new PBEKeySpec(pass);
+        SecretKey skey = fac.generateSecret(spec);
+        KeySpec spec1 = fac.getKeySpec(skey, PBEKeySpec.class);
+        SecretKey skey1 = fac.generateSecret(spec1);
+        assertEquals(skey, skey1);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256", "PBEWithHmacSHA512/224AndAES_128", "PBEWithHmacSHA512/224AndAES_256",
+        "PBEWithHmacSHA512/256AndAES_128", "PBEWithHmacSHA512/256AndAES_256"})
+    public void testInvalid(String algorithm) throws Exception {
+
+        SecretKeyFactory fac = SecretKeyFactory.getInstance(algorithm, getProviderName());
+
+        char[] pass = new char[] {'p', 'a', 's', 's', 'w', 'o', 'r', '\u0019'};
+        PBEKeySpec spec = new PBEKeySpec(pass);
+        try {
+            SecretKey skey = fac.generateSecret(spec);
+            fail("Expected InvalidKeySpecException for algorithm: " + skey.getAlgorithm() + " but none was thrown.");
+        } catch (InvalidKeySpecException e) {
+            assertEquals("Invalid Password.", e.getMessage());
+        }
+    }
+}
