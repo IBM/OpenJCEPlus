19577d98404d4edbb4d3f0ff523bd2ec025b219f
Sabrina Lee <sabrinalee@ibm.com>
2025-10-03 14:32:56 -0400

    Implement cleaner to replace deprecated finalizers
    The cleaner is the replacement for the deprecated finalize() method for
    cleaning objects before garbage collection.
    
    Each class that was previously using finalize() will have an anonymous
    function that cleans up the necessary resources. The number of cleaner
    threads can be set by a property with the default value being 2. The
    object and its anonymous function are registered to one of the cleaner
    threads in round robin order.
    
    Signed-off-by: Sabrina Lee <sabrinalee@ibm.com>

--- src/main/java/com/ibm/crypto/plus/provider/OpenJCEPlusProvider.java
+++ src/main/java/com/ibm/crypto/plus/provider/OpenJCEPlusProvider.java
@@ -9,7 +9,10 @@
 package com.ibm.crypto.plus.provider;
 
 import com.ibm.crypto.plus.provider.ock.OCKContext;
+import java.lang.ref.Cleaner;
 import java.security.ProviderException;
+import java.util.concurrent.atomic.AtomicInteger;
+import sun.security.util.Debug;
 
 // Internal interface for OpenJCEPlus and OpenJCEPlus implementation classes.
 // Implemented as an abstract class rather than an interface so that 
@@ -31,8 +34,29 @@ public abstract class OpenJCEPlusProvider extends java.security.Provider {
     //    private static boolean verifiedSelfIntegrity = false;
     private static final boolean verifiedSelfIntegrity = true;
 
+    private final Cleaner[] cleaners;
+
+    private final int DEFAULT_NUM_CLEANERS = 2;
+
+    private final int numCleaners;
+
+    private AtomicInteger count = new AtomicInteger(0);
+
+    private static final boolean isDebugSet = Debug.getInstance(DEBUG_VALUE) != null ? true : false;
+
     OpenJCEPlusProvider(String name, String info) {
         super(name, PROVIDER_VER, info);
+
+        numCleaners = Integer.getInteger("openjceplus.cleaners.num", DEFAULT_NUM_CLEANERS);
+        if (numCleaners < 1){
+            throw new IllegalArgumentException(numCleaners + " is an invalid number of cleaner threads, must be at least 1.");
+        }
+
+        cleaners = new Cleaner[numCleaners];
+        for (int i = 0; i < numCleaners; i++) {
+            final Cleaner cleaner = Cleaner.create();
+            cleaners[i] = cleaner;
+        }
     }
 
     static final boolean verifySelfIntegrity(Object c) {
@@ -47,6 +71,15 @@ public abstract class OpenJCEPlusProvider extends java.security.Provider {
         return true;
     }
 
+    public void registerCleanable(Object owner, Runnable cleanAction) {
+        Cleaner cleaner = cleaners[Math.abs(count.getAndIncrement() % numCleaners)];
+        cleaner.register(owner, cleanAction);
+    }
+
+    public boolean getDebug() {
+        return isDebugSet;
+    }
+    
     // Get OCK context for crypto operations
     //
     abstract OCKContext getOCKContext();
