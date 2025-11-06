09d40624ced6b34b0e048c56f0862b5b5e7e428d
Dev Agarwal <124221073+dev-koan@users.noreply.github.com>
2025-11-06 16:04:22 -0500

    Support for PBES2, PBEKeyfactory, PBEKey and PBEParameters (#898)
    This update adds support for PBES2, PBEKeyFactory, PBEKey,
    and PBEParameters for PBE algorithms with Hmac and AES for
    OpenJCEPlus provider.
    
    Signed-off-by: Dev Agarwal <dev.agarwal@ibm.com>

---
+++ src/test/java/ibm/jceplus/junit/openjceplus/TestPBEKeyFactoryInterop.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright IBM Corp. 2025
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms provided by IBM in the LICENSE file that accompanied
+ * this code, including the "Classpath" Exception described therein.
+ */
+
+package ibm.jceplus.junit.openjceplus;
+
+import ibm.jceplus.junit.base.BaseTestPBEKeyFactoryInterop;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class TestPBEKeyFactoryInterop extends BaseTestPBEKeyFactoryInterop {
+
+    @BeforeAll
+    public void beforeAll() {
+        Utils.loadProviderTestSuite();
+        setProviderName(Utils.TEST_SUITE_PROVIDER_NAME);
+        setInteropProviderName(Utils.PROVIDER_SunJCE);
+    }
+}
