bf71a4e1b20006282591130671e92547611e2b3e
Dev Agarwal <124221073+dev-koan@users.noreply.github.com>
2025-11-06 16:04:30 -0500

    Support for PBES2, PBEKeyfactory, PBEKey and PBEParameters (#900)
    This update adds support for PBES2, PBEKeyFactory, PBEKey,
    and PBEParameters for PBE algorithms with Hmac and AES for
    OpenJCEPlus provider.
    
    Signed-off-by: Dev Agarwal <dev.agarwal@ibm.com>

---
+++ src/main/java/com/ibm/crypto/plus/provider/PBEKeyFactory.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright IBM Corp. 2025
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms provided by IBM in the LICENSE file that accompanied
+ * this code, including the "Classpath" Exception described therein.
+ */
+
+package com.ibm.crypto.plus.provider;
+
+import java.security.InvalidKeyException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactorySpi;
+import javax.crypto.spec.PBEKeySpec;
+
+abstract class PBEKeyFactory extends SecretKeyFactorySpi {
+
+    private final String type;
+    private OpenJCEPlusProvider provider = null;
+
+    private PBEKeyFactory(String keytype, OpenJCEPlusProvider provider) {
+        type = keytype;
+        this.provider = provider;
+    }
+
+    public static final class PBEWithHmacSHA1AndAES_128 extends PBEKeyFactory {
+        public PBEWithHmacSHA1AndAES_128(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA1AndAES_128", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA224AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA224AndAES_128(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA224AndAES_128", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA256AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA256AndAES_128(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA256AndAES_128", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA384AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA384AndAES_128(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA384AndAES_128", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA512AndAES_128
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA512AndAES_128(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA512AndAES_128", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA1AndAES_256 extends PBEKeyFactory {
+        public PBEWithHmacSHA1AndAES_256(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA1AndAES_256", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA224AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA224AndAES_256(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA224AndAES_256", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA256AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA256AndAES_256(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA256AndAES_256", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA384AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA384AndAES_256(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA384AndAES_256", provider);
+        }
+    }
+
+    public static final class PBEWithHmacSHA512AndAES_256
+            extends PBEKeyFactory {
+        public PBEWithHmacSHA512AndAES_256(OpenJCEPlusProvider provider) {
+            super("PBEWithHmacSHA512AndAES_256", provider);
+        }
+    }
+
+    /**
+     * Generates a <code>SecretKey</code> object from the provided key
+     * specification (key material).
+     *
+     * @param keySpec the specification (key material) of the secret key
+     *
+     * @return the secret key
+     *
+     * @exception InvalidKeySpecException if the given key specification
+     * is inappropriate for this key factory to produce a public key.
+     */
+    protected SecretKey engineGenerateSecret(KeySpec keySpec)
+        throws InvalidKeySpecException
+    {
+        if (!(keySpec instanceof PBEKeySpec)) {
+            throw new InvalidKeySpecException("Invalid key spec");
+        }
+        return new PBEKey(provider, (PBEKeySpec) keySpec, type);
+    }
+
+    /**
+     * Returns a specification (key material) of the given key
+     * in the requested format.
+     *
+     * @param key the key
+     *
+     * @param keySpecCl the requested format in which the key material shall be
+     * returned
+     *
+     * @return the underlying key specification (key material) in the
+     * requested format
+     *
+     * @exception InvalidKeySpecException if the requested key specification is
+     * inappropriate for the given key, or the given key cannot be processed
+     * (e.g., the given key has an unrecognized algorithm or format).
+     */
+    protected KeySpec engineGetKeySpec(SecretKey key, Class<?> keySpecCl)
+        throws InvalidKeySpecException {
+        if ((key != null) && (key.getFormat() != null) && (key.getFormat().equalsIgnoreCase("RAW")) && (key.getEncoded() != null)) {
+            // Check if requested key spec is amongst the valid ones
+            if ((keySpecCl != null) && keySpecCl.isAssignableFrom(PBEKeySpec.class)) {
+                byte[] passwdBytes = key.getEncoded();
+                char[] passwdChars = new char[passwdBytes.length];
+                for (int i=0; i < passwdChars.length; i++)
+                    passwdChars[i] = (char) (passwdBytes[i] & 0x7f);
+                PBEKeySpec ret = new PBEKeySpec(passwdChars);
+                // password char[] was cloned in PBEKeySpec constructor,
+                // so we can zero it out here
+                java.util.Arrays.fill(passwdChars, '\0');
+                java.util.Arrays.fill(passwdBytes, (byte) 0x00);
+                return ret;
+            } else {
+                throw new InvalidKeySpecException("Invalid key spec");
+            }
+        } else {
+            throw new InvalidKeySpecException("Invalid key "
+                                            + "format/algorithm");
+        }
+    }
+
+    /**
+     * Translates a <code>SecretKey</code> object, whose provider may be
+     * unknown or potentially untrusted, into a corresponding
+     * <code>SecretKey</code> object of this key factory.
+     *
+     * @param key the key whose provider is unknown or untrusted
+     *
+     * @return the translated key
+     *
+     * @exception InvalidKeyException if the given key cannot be processed by
+     * this key factory.
+     */
+    protected SecretKey engineTranslateKey(SecretKey key)
+        throws InvalidKeyException
+    {
+        try {
+            if ((key != null) && (key.getFormat() != null) && (key.getFormat().equalsIgnoreCase("RAW"))) {
+
+                if (key instanceof com.ibm.crypto.plus.provider.PBEKey) {
+                    return key;
+                }
+
+                // Convert key to spec
+                PBEKeySpec pbeKeySpec = (PBEKeySpec) engineGetKeySpec
+                    (key, PBEKeySpec.class);
+
+                try {
+                    return engineGenerateSecret(pbeKeySpec);
+                } finally {
+                    pbeKeySpec.clearPassword();
+                }
+            } else {
+                throw new InvalidKeyException("Invalid key format/algorithm");
+            }
+
+        } catch (InvalidKeySpecException ikse) {
+            throw new InvalidKeyException("Cannot translate key: "
+                                          + ikse.getMessage());
+        }
+    }
+}
---
+++ src/test/java/ibm/jceplus/junit/base/BaseTestPBEKeyFactoryInterop.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright IBM Corp. 2025
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms provided by IBM in the LICENSE file that accompanied
+ * this code, including the "Classpath" Exception described therein.
+ */
+
+package ibm.jceplus.junit.base;
+
+import java.security.SecureRandom;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Tests to perform interoperability tests between a provider under test,
+ * typically OpenJCEPlus or OpenJCEPlusFIPS and another provider for
+ * PBE supported algorithms.
+ */
+public class BaseTestPBEKeyFactoryInterop extends BaseTestJunit5Interop {
+
+    final String PASSWORD = "Thequickbrownfoxjumpsoverthelazydog";
+    byte[] randomSalt = new byte[32];
+    SecureRandom random = new SecureRandom();
+    PBEKeySpec pbeks = null;
+
+    @BeforeAll
+    public void setUp() {
+        random.nextBytes(randomSalt);
+        this.pbeks = new PBEKeySpec(PASSWORD.toCharArray());
+    }
+    /**
+     * Test used to perform interoperability tests using a KeyFactory for
+     * the method `getAlgorithm()`.
+     */
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256"})
+    public void testGetAlgorithm(String algorithm) throws Exception {
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+        SecretKeyFactory skfInterop = SecretKeyFactory.getInstance(algorithm,
+                this.getInteropProviderName());
+
+        // Validate that the algorithm name from provider under test matches the interop provider.
+        System.out.println("    Checking getAlgorithm()");
+        assertEquals(skf.getAlgorithm(), skfInterop.getAlgorithm(),
+                "Algorithm name is not as expected.");
+    }
+
+    /**
+     * Test used to perform interoperability tests using a KeyFactory for
+     * the method `getEncoded()`.
+     */
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256"})
+    public void testGetEncoding(String algorithm) throws Exception {
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+        SecretKeyFactory skfInterop = SecretKeyFactory.getInstance(algorithm,
+                this.getInteropProviderName());
+
+        // Validate key encodings generated from provider under test matches the interop provider.
+        SecretKey sk1 = skf.generateSecret(pbeks);
+        SecretKey skInterop = skfInterop.generateSecret(pbeks);
+        assertArrayEquals(sk1.getEncoded(), skInterop.getEncoded(), "Key encodings do not match.");
+    }
+
+    /**
+     * Test used to perform interoperability tests using a KeyFactory for
+     * the method `translateKey()`.
+     */
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256"})
+    public void testTranslate(String algorithm) throws Exception {
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+        SecretKeyFactory skfInterop = SecretKeyFactory.getInstance(algorithm,
+                this.getInteropProviderName());
+
+        // Validate key translations of the same key generated from provider under test matches the interop provider.
+        System.out.println("    Checking translateKey()");
+        SecretKey sk1 = skf.generateSecret(pbeks);
+        SecretKey skInterop = skfInterop.generateSecret(pbeks);
+        SecretKey sk1T = skf.translateKey(sk1);
+        SecretKey skInteropT = skf.translateKey(skInterop);
+        assertArrayEquals(sk1T.getEncoded(), skInteropT.getEncoded(),
+                "Translated keys do not match.");
+        SecretKey sk1TI = skfInterop.translateKey(sk1);
+        SecretKey skInteropTI = skfInterop.translateKey(skInterop);
+        assertArrayEquals(sk1TI.getEncoded(), skInteropTI.getEncoded(),
+                "Translated keys do not match.");
+    }
+
+    /**
+     * Test used to perform interoperability tests using a KeyFactory for
+     * the method `getKeySpec()`.
+     */
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256"})
+    public void testKeySpec(String algorithm) throws Exception {
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+        SecretKeyFactory skfInterop = SecretKeyFactory.getInstance(algorithm,
+                this.getInteropProviderName());
+
+        // Validate that the key spec produced by the provider under test matches the interop provider.
+        System.out.println("    Checking getKeySpec()");
+        SecretKey sk1 = skf.generateSecret(pbeks);
+        SecretKey skInterop = skfInterop.generateSecret(pbeks);
+        PBEKeySpec ks1 = (PBEKeySpec) skf.getKeySpec(sk1, PBEKeySpec.class);
+        PBEKeySpec ksInterop = (PBEKeySpec) skfInterop.getKeySpec(skInterop, PBEKeySpec.class);
+        assertEquals(ks1.getIterationCount(), ksInterop.getIterationCount(),
+                "Iteration count does not match.");
+        assertEquals(ks1.getKeyLength(), ksInterop.getKeyLength(), "Key length does not match.");
+        assertArrayEquals(ks1.getPassword(), ksInterop.getPassword(), "Password does not match.");
+        assertArrayEquals(ks1.getSalt(), ksInterop.getSalt(), "Salt does not match.");
+    }
+
+    /**
+     * Test used to perform interoperability tests using a KeyFactory for
+     * the method `hashCode()`.
+     */
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256"})
+    public void testHashCode(String algorithm) throws Exception {
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+        SecretKeyFactory skfInterop = SecretKeyFactory.getInstance(algorithm,
+                this.getInteropProviderName());
+        SecretKey sk1 = skf.generateSecret(pbeks);
+        SecretKey skInterop = skfInterop.generateSecret(pbeks);
+        assertEquals(sk1.hashCode(), skInterop.hashCode(), "Hash codes do not match." + algorithm);
+    }
+
+    /**
+     * Test used to perform interoperability tests using a KeyFactory for
+     * the method `equals()`.
+     */
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256"})
+    public void testEquality(String algorithm) throws Exception {
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+        SecretKeyFactory skfInterop = SecretKeyFactory.getInstance(algorithm,
+                this.getInteropProviderName());
+        SecretKey sk1 = skf.generateSecret(pbeks);
+        SecretKey skInterop = skfInterop.generateSecret(pbeks);
+        assertTrue(sk1.equals(skInterop), "Keys are not equal between different providers." + algorithm);
+        assertTrue(sk1.equals(sk1), "Keys are not equal when key is exactly the same.");
+
+        PBEKeySpec pbeksDifferent = new PBEKeySpec("DifferentPW".toCharArray());
+        SecretKey skDifferent = skf.generateSecret(pbeksDifferent);
+        assertFalse(sk1.equals(skDifferent), "Keys are not expected to be equal.");
+    }
+
+    /**
+     * Test used to perform interoperability tests using a SecretKey generated
+     * by a providers KeyFactory for the method `getFormat()`.
+     */
+    @ParameterizedTest
+    @CsvSource({"PBEWithHmacSHA1AndAES_128", "PBEWithHmacSHA1AndAES_256", "PBEWithHmacSHA224AndAES_128", "PBEWithHmacSHA224AndAES_256",
+        "PBEWithHmacSHA256AndAES_128", "PBEWithHmacSHA256AndAES_256", "PBEWithHmacSHA384AndAES_128", "PBEWithHmacSHA384AndAES_256",
+        "PBEWithHmacSHA512AndAES_128", "PBEWithHmacSHA512AndAES_256"})
+    public void testGetFormat(String algorithm) throws Exception {
+
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm, this.getProviderName());
+        SecretKeyFactory skfInterop = SecretKeyFactory.getInstance(algorithm,
+                this.getInteropProviderName());
+        SecretKey sk1 = skf.generateSecret(pbeks);
+        SecretKey skInterop = skfInterop.generateSecret(pbeks);
+        assertEquals(sk1.getFormat(), skInterop.getFormat(), "Format does not match.");
+    }
+}
