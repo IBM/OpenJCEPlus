de297fd00561c6f2c9558c4db39bf4d8a15fe6d4
Sabrina Lee <sabrinalee@ibm.com>
2025-11-25 09:37:27 -0500

    Fix compilation warnings
    This fix resolves warnings that appear when building OpenJCEPlus related
    to deprecated and missing constructors, as well as deprecated default
    values used in instantiation.
    
    Signed-off-by: Sabrina Lee <sabrinalee@ibm.com>

--- src/main/java/com/ibm/crypto/plus/provider/PrimitiveWrapper.java
+++ src/main/java/com/ibm/crypto/plus/provider/PrimitiveWrapper.java
@@ -19,6 +19,9 @@ package com.ibm.crypto.plus.provider;
  */
 
 public final class PrimitiveWrapper {
+
+    public PrimitiveWrapper() {}
+    
     public static class Long { 
         long value;
 
