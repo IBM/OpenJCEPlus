f20e8027b728fa41a70224de2d9cdd001c1f08dd
Kostas Tsiounis <kostas.tsiounis@ibm.com>
2025-12-02 15:56:04 -0500

    Restrict RSA paddings allowed through OpenJCEPlusFIPS
    This change adds attributes in the RSA service registration
    in the OpenJCEPlusFIPS provider to only support OAEP paddings.
    
    It, also, update the engineSetPadding() method to only allow
    OAEP paddings to be set when the FIPS provider is used.
    
    Tests are updated accordingly to skip invalid padding tests
    when running with OpenJCEPlusFIPS.
    
    A temporary flag that allows the use of other paddings with
    OpenJCEPlusFIPS is introduced to facilitate migration of
    users utilizing the previous behaviour. The flag to be set
    to revert this behaviour is -Dcom.ibm.openjceplusfips.allowNonOAEP=true.
    
    Signed-off-by: Kostas Tsiounis <kostas.tsiounis@ibm.com>

--- src/test/java/ibm/jceplus/junit/base/BaseTestRSAKeyInteropBC.java
+++ src/test/java/ibm/jceplus/junit/base/BaseTestRSAKeyInteropBC.java
@@ -30,6 +30,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 public class BaseTestRSAKeyInteropBC extends BaseTestJunit5Interop {
 
@@ -500,6 +501,7 @@ public class BaseTestRSAKeyInteropBC extends BaseTestJunit5Interop {
 
     @Test
     public void testEncryptPlusDecryptBC() {
+        assumeFalse("OpenJCEPlusFIPS".equals(getProviderName()));
 
         try {
 
@@ -543,6 +545,8 @@ public class BaseTestRSAKeyInteropBC extends BaseTestJunit5Interop {
 
     @Test
     public void testEncryptBCDecryptPlus() {
+        assumeFalse("OpenJCEPlusFIPS".equals(getProviderName()));
+
         byte[] msgBytes = ("This is a short message".getBytes());
         //long message to be encrypted and decrypted using RSA public and Private key;" + 
         //        "encrypt with BC and decrypt with BCPlus and vice versa").getBytes();
