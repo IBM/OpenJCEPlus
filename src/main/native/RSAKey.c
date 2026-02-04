IBM/OpenJCEPlus

commit 46e83bda482a23442fcb88b184beca5e86533992

Migrate to latest RSA EVP APIs for key generation (#1113)
This update migrates to the latest RSA EVP APIs that are available in
OCKC.

Signed-off-by: Jason Katonica <katonica@us.ibm.com>

src/main/native/RSAKey.c
@@ -28,30 +28,131 @@ Java_com_ibm_crypto_plus_provider_ock_NativeInterface_RSAKEY_1generate(
     JNIEnv *env, jclass thisObj, jlong ockContextId, jint numBits, jlong e) {
     static const char *functionName = "NativeInterface.RSAKEY_generate";
 
-    ICC_CTX *ockCtx   = (ICC_CTX *)((intptr_t)ockContextId);
-    ICC_RSA *ockRSA   = NULL;
-    jlong    rsaKeyId = 0;
+    ICC_CTX          *ockCtx   = (ICC_CTX *)((intptr_t)ockContextId);
+    ICC_RSA          *ockRSA   = NULL;
+    ICC_EVP_PKEY     *ockPKey  = NULL;
+    ICC_EVP_PKEY_CTX *pctx     = NULL;
+    jlong             rsaKeyId = 0;
+    int               ret      = 0;
+    char              bitStr[32];
+    char              expStr[32];
 
     if (debug) {
         gslogFunctionEntry(functionName);
     }
 
-    ockRSA = ICC_RSA_generate_key(ockCtx, (int)numBits, (long)e, NULL, NULL);
-    if (ockRSA == NULL) {
+    pctx = ICC_EVP_PKEY_CTX_new_id(ockCtx, ICC_EVP_PKEY_RSA, NULL);
+    if (pctx == NULL) {
 #ifdef DEBUG_RSA_DETAIL
         if (debug) {
-            gslogMessage("DETAIL_RSA  FAILURE ICC_RSA_generate_key");
+            gslogMessage("DETAIL_RSA FAILURE ICC_EVP_PKEY_CTX_new_id");
         }
 #endif
         ockCheckStatus(ockCtx);
-        throwOCKException(env, 0, "ICC_RSA_generate_key() failed");
-    } else {
-        rsaKeyId = (jlong)((intptr_t)ockRSA);
+        throwOCKException(env, 0, "ICC_EVP_PKEY_CTX_new_id() failed");
+        goto cleanup;
+    }
+
+    ret = ICC_EVP_PKEY_keygen_init(ockCtx, pctx);
+    if (ret != ICC_OSSL_SUCCESS) {
 #ifdef DEBUG_RSA_DETAIL
         if (debug) {
-            gslogMessage("DETAIL_RSA  rsaKeyId %lx", rsaKeyId);
+            gslogMessage("DETAIL_RSA FAILURE ICC_EVP_PKEY_keygen_init");
         }
 #endif
+        ockCheckStatus(ockCtx);
+        throwOCKException(env, 0, "ICC_EVP_PKEY_keygen_init() failed");
+        goto cleanup;
+    }
+
+    snprintf(bitStr, sizeof(bitStr), "%d", (int)numBits);
+#ifdef __MVS__
+#pragma convert("ISO8859-1")
+#endif
+    ret = ICC_EVP_PKEY_CTX_ctrl_str(ockCtx, pctx, "rsa_keygen_bits", bitStr);
+#ifdef __MVS__
+#pragma convert(pop)
+#endif
+    if (ret <= 0) {
+#ifdef DEBUG_RSA_DETAIL
+        if (debug) {
+            gslogMessage(
+                "DETAIL_RSA FAILURE ICC_EVP_PKEY_CTX_ctrl_str (set bits)");
+        }
+#endif
+        ockCheckStatus(ockCtx);
+        throwOCKException(env, 0,
+                          "ICC_EVP_PKEY_CTX_ctrl_str() failed to set key size");
+        goto cleanup;
+    }
+
+    snprintf(expStr, sizeof(expStr), "%ld", (long)e);
+#ifdef __MVS__
+#pragma convert("ISO8859-1")
+#endif
+    ret = ICC_EVP_PKEY_CTX_ctrl_str(ockCtx, pctx, "rsa_keygen_pubexp", expStr);
+#ifdef __MVS__
+#pragma convert(pop)
+#endif
+    if (ret <= 0) {
+#ifdef DEBUG_RSA_DETAIL
+        if (debug) {
+            gslogMessage(
+                "DETAIL_RSA FAILURE ICC_EVP_PKEY_CTX_ctrl_str (set pubexp)");
+        }
+#endif
+        ockCheckStatus(ockCtx);
+        throwOCKException(
+            env, 0,
+            "ICC_EVP_PKEY_CTX_ctrl_str() failed to set public exponent");
+        goto cleanup;
+    }
+
+    ret = ICC_EVP_PKEY_keygen(ockCtx, pctx, &ockPKey);
+    if (ret != ICC_OSSL_SUCCESS || ockPKey == NULL) {
+#ifdef DEBUG_RSA_DETAIL
+        if (debug) {
+            gslogMessage("DETAIL_RSA FAILURE ICC_EVP_PKEY_keygen");
+        }
+#endif
+        ockCheckStatus(ockCtx);
+        throwOCKException(env, 0, "ICC_EVP_PKEY_keygen() failed");
+        goto cleanup;
+    }
+
+    ockRSA = ICC_EVP_PKEY_get1_RSA(ockCtx, ockPKey);
+    if (ockRSA == NULL) {
+#ifdef DEBUG_RSA_DETAIL
+        if (debug) {
+            gslogMessage("DETAIL_RSA FAILURE ICC_EVP_PKEY_get1_RSA");
+        }
+#endif
+        ockCheckStatus(ockCtx);
+        throwOCKException(env, 0, "ICC_EVP_PKEY_get1_RSA() failed");
+        goto cleanup;
+    }
+
+    rsaKeyId = (jlong)((intptr_t)ockRSA);
+#ifdef DEBUG_RSA_DETAIL
+    if (debug) {
+        gslogMessage("DETAIL_RSA rsaKeyId %lx", rsaKeyId);
+    }
+#endif
+
+cleanup:
+    if (pctx != NULL) {
+        ICC_EVP_PKEY_CTX_free(ockCtx, pctx);
+        pctx = NULL;
+    }
+
+    if (ockPKey != NULL) {
+        ICC_EVP_PKEY_free(ockCtx, ockPKey);
+        ockPKey = NULL;
+    }
+
+    if ((ockRSA != NULL) && (rsaKeyId == 0)) {
+        ICC_RSA_free(ockCtx, ockRSA);
+        ockRSA = NULL;
     }
 
     if (debug) {

commit 270d8c385408b2d79cf045a91ea2aec96dd3429f

Fix encoding issue in RSAKey.c for z/OS
This update fixes an encoding issue in RSAKey.c for z/OS that was caused by #1113.

See: #1142

Signed-off-by: Thomas-Ginader <thomas.ginader@ibm.com>

src/main/native/RSAKey.c
@@ -66,6 +66,11 @@ Java_com_ibm_crypto_plus_provider_ock_NativeInterface_RSAKEY_1generate(
     }
 
     snprintf(bitStr, sizeof(bitStr), "%d", (int)numBits);
+
+#ifdef __MVS__
+    forceToAscii(bitStr); // Ensure the value is ASCII before passing to OCK
+#endif
+
 #ifdef __MVS__
 #pragma convert("ISO8859-1")
 #endif
@@ -87,6 +92,11 @@ Java_com_ibm_crypto_plus_provider_ock_NativeInterface_RSAKEY_1generate(
     }
 
     snprintf(expStr, sizeof(expStr), "%ld", (long)e);
+
+#ifdef __MVS__
+    forceToAscii(expStr); // Ensure the value is ASCII before passing to OCK
+#endif
+
 #ifdef __MVS__
 #pragma convert("ISO8859-1")
 #endif

src/main/native/Utils.c
@@ -248,5 +248,17 @@ void throwOCKException(JNIEnv *env, int code, const char *msg) {
 }
 
 #ifdef __MVS__
+/* Manual EBCDIC to ASCII conversion for digits */
+void forceToAscii(char *s) {
+    while (*s) {
+        if (*s >= (char)0xF0 && *s <= (char)0xF9) {
+            *s = *s - (char)0xF0 + (char)0x30;
+        }
+        s++;
+    }
+}
+#endif
+
+#ifdef __MVS__
 #include "closed_Utils_c.h"
 #endif

src/main/native/Utils.h
@@ -52,4 +52,8 @@ void ockCheckStatus(ICC_CTX* ctx);
 
 void throwOCKException(JNIEnv* env, int code, const char* msg);
 
+#ifdef __MVS__
+void forceToAscii(char* s);
+#endif
+
 #endif