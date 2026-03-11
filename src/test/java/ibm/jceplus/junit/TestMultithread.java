232714dcf56a7cd6ff8ef5076f6c30fc23756463
Mohit Rajbhar <mohit.rajbhar@ibm.com>
2026-03-11 23:39:50 +0530

    Refactor JUnit 5 for BaseTestAESCopySafe and BaseTestAESGCMLong tests (#1246)
    This change consolidates the tests previously associated with BaseTestAESCopySafe into the parameterized TestAESCopySafe class, and the tests from BaseTestAESGCMLong into the parameterized TestAESGCMLong class.
    
    Signed-off-by: Mohit Rajbhar <mohit.rajbhar@ibm.com>

--- src/test/java/ibm/jceplus/junit/TestMultithread.java
+++ src/test/java/ibm/jceplus/junit/TestMultithread.java
@@ -31,8 +31,6 @@ public class TestMultithread {
     private final int numThreads = 10;
     private final int timeoutSec = 4500;
     private final String[] testList = {
-            "ibm.jceplus.junit.openjceplus.multithread.TestAESCopySafe",
-            "ibm.jceplus.junit.openjceplus.multithread.TestAESGCMLong",
             "ibm.jceplus.junit.openjceplus.multithread.TestAESGCMNonExpanding",
             "ibm.jceplus.junit.openjceplus.multithread.TestAESGCMSameBuffer",
             "ibm.jceplus.junit.openjceplus.multithread.TestAESGCMUpdate",
