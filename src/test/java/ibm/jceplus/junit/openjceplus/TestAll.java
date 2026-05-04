aa96fb9abe047ce755660fb8cbdddd8b9fd0f18e
Mohit Rajbhar <mohit.rajbhar@ibm.com>
2026-05-04 12:44:21 +0530

    Refactor Junit5 for BaseTestSHA3_224KAT and BaseTestSHA3_256KAT tests
    This change consolidates the tests previously associated with BaseTestSHA3_224KAT into the parameterized TestSHA3_224KAT test class, and the tests from BaseTestSHA3_256KAT into the parameterized TestSHA3_256KAT test class.
    
    Signed-off-by: Mohit Rajbhar <mohit.rajbhar@ibm.com>

--- src/test/java/ibm/jceplus/junit/openjceplus/TestAll.java
+++ src/test/java/ibm/jceplus/junit/openjceplus/TestAll.java
@@ -108,8 +108,6 @@ import org.junit.platform.suite.api.Suite;
     TestSHA1.class,
     TestSHA224.class,
     TestSHA256.class,
-    TestSHA3_224.class,
-    TestSHA3_256.class,
     TestSHA384.class,
     TestSHA512_224.class,
     TestSHA512_256.class,
--- src/test/java/ibm/jceplus/junit/tests/TestSHA3_224KAT.java
+++ src/test/java/ibm/jceplus/junit/tests/TestSHA3_224KAT.java
@@ -1,20 +1,35 @@
 /*
- * Copyright IBM Corp. 2023, 2025
+ * Copyright IBM Corp. 2023, 2026
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms provided by IBM in the LICENSE file that accompanied
  * this code, including the "Classpath" Exception described therein.
  */
 
-package ibm.jceplus.junit.base;
+package ibm.jceplus.junit.tests;
 
 import java.security.MessageDigest;
 import java.util.Arrays;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.Parameter;
+import org.junit.jupiter.params.ParameterizedClass;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class BaseTestSHA3_224KAT extends BaseTestMessageDigest {
+@Tag(Tags.OPENJCEPLUS_NAME)
+@Tag(Tags.OPENJCEPLUS_FIPS_NAME)
+@Tag(Tags.OPENJCEPLUS_MULTITHREAD_NAME)
+@Tag(Tags.OPENJCEPLUS_FIPS_MULTITHREAD_NAME)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ParameterizedClass
+@MethodSource("ibm.jceplus.junit.tests.TestArguments#getEnabledProviders")
+public class TestSHA3_224KAT extends BaseTestMessageDigest {
+
+    @Parameter(0)
+    TestProvider provider;
 
     final String[][] tests = {{
             "31c82d71785b7ca6b651cb6c8c9ad5e2aceb0b0633c088d33aa247ada7a594ff4936c023251319820a9b19fc6c48de8a6f7ada214176ccdaadaeef51ed43714ac0c8269bbd497e46e78bb5e58196494b2471b1680e2d4c6dbd249831bd83a4d3be06c8a2e903933974aa05ee748bfe6ef359f7a143edf0d4918da916bd6f15e26a790cff514b40a5da7f72e1ed2fe63a05b8149587bea05653718cc8980eadbfeca85b7c9c286dd040936585938be7f98219700c83a9443c2856a80ff46852b26d1b1edf72a30203cf6c44a10fa6eaf1920173cedfb5c4cf3ac665b37a86ed02155bbbf17dc2e786af9478fe0889d86c5bfa85a242eb0854b1482b7bd16f67f80bef9c7a628f05a107936a64273a97b0088b0e515451f916b5656230a12ba6dc78",
@@ -495,8 +510,9 @@ public class BaseTestSHA3_224KAT extends BaseTestMessageDigest {
             {"e65de91fdcb7606f14dbcfc94c9c94a57240a6b2c31ed410346c4dc011526559e44296fc988cc589de2dc713d0e82492d4991bd8c4c5e6c74c753fc09345225e1db8d565f0ce26f5f5d9f404a28cf00bd655a5fe04edb682942d675b86235f235965ad422ba5081a21865b8209ae81763e1c4c0cccbccdaad539cf773413a50f5ff1267b9238f5602adc06764f775d3c",
                     "26ec9df54d9afe11710772bfbeccc83d9d0439d3530777c81b8ae6a3"}};
 
-    @BeforeAll
-    public void setUp() {
+    @BeforeEach
+    public void setUp() throws Exception {
+        setAndInsertProvider(provider);
         setAlgorithm("SHA3-224");
     }
 
