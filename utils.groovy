989419f89ec1daa02eb2c1e7b784f5a441d542ca
Kostas Tsiounis <kostas.tsiounis@ibm.com>
2026-05-22 15:05:38 -0400

    Update Jenkins pipeline 5

--- utils.groovy
+++ utils.groovy
@@ -404,7 +404,7 @@ return [
     getBinaries: this.&getBinaries,
     getJava: this.&getJava,
     getMaven: this.&getMaven,
-    getOpenSSL: this.&getOpenSSL
+    getOpenSSL: this.&getOpenSSL,
     cloneOpenJCEPlus: this.&cloneOpenJCEPlus,
     runOpenJCEPlus: this.&runOpenJCEPlus,
     upload_artifactory: this.&upload_artifactory,
