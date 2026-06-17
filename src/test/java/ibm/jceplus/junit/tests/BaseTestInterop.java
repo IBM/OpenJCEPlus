6fee92247a31819696ce57e216069a75e518e1a4
Mohit Rajbhar <mohit.rajbhar@ibm.com>
2026-06-17 22:03:02 +0530

    Refactor JUnit 5 Tests for BaseTestDSASignatureInterop (#1468)
    Replace multiple test classes extending BaseTestDSASignatureInterop with a single JUnit 5 parameterized test class
    
    Signed-off-by: Mohit Rajbhar <mohit.rajbhar@ibm.com>

---
+++ src/test/java/ibm/jceplus/junit/tests/BaseTestInterop.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright IBM Corp. 2026
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms provided by IBM in the LICENSE file that accompanied
+ * this code, including the "Classpath" Exception described therein.
+ */
+
+package ibm.jceplus.junit.tests;
+
+public class BaseTestInterop extends BaseTest {
+
+    public String interopProviderName;
+    public String interopProviderName2;
+
+    /**
+     * Sets the provider name to interop with.
+     *
+     * @param providerName the provider name associated with this test case for use.
+     */
+    public void setInteropProviderName(String providerName) {
+        this.interopProviderName = providerName;
+    }
+
+    /**
+     * Sets the interop provider name and loads the provider.
+     *
+     * @param interopProvider the provider to be used for interop.
+     */
+    public void setAndInsertInteropProvider(TestProvider interopProvider) throws Exception {
+        this.interopProviderName = interopProvider.getProviderName();
+        loadSupportedProvider(interopProvider);
+    }
+
+    /**
+     * Gets the provider name that is to be used for interop.
+     *
+     * @return The provider name associated with the interop provider name.
+     */
+    public String getInteropProviderName() {
+        return this.interopProviderName;
+    }
+
+    /**
+     * Sets the provider name to interop with.
+     *
+     * @param providerName the provider name associated with this test case for use.
+     */
+    public void setInteropProviderName2(String providerName) {
+        this.interopProviderName2 = providerName;
+    }
+
+    /**
+     * Gets the provider name that is to be used for interop.
+     *
+     * @return The provider name associated with the interop provider name.
+     */
+    public String getInteropProviderName2() {
+        return this.interopProviderName2;
+    }
+}
