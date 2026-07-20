7aca335c2bd828ad8c9f7253b74d5bc4dc34b901
Mohit Rajbhar <mohit.rajbhar@ibm.com>
2026-07-20 22:42:51 +0530

    Refactor SHA message digest tests into JUnit 5 parameterized tests (#1635)
    Convert SHA message digest test implementations to JUnit 5 parameterized
    tests and remove duplicate provider-specific test code.
    
    Changes:
    
    - Convert BaseTestSHA1.java to a JUnit 5 parameterized test
    - Convert BaseTestSHA224.java to a JUnit 5 parameterized test
    - Convert BaseTestSHA256.java to a JUnit 5 parameterized test
    - Convert BaseTestSHA384.java to a JUnit 5 parameterized test
    - Convert BaseTestSHA512.java to a JUnit 5 parameterized test
    - Convert BaseTestSHA512_224.java to a JUnit 5 parameterized test
    - Convert BaseTestSHA512_256.java to a JUnit 5 parameterized test
    - Remove provider-specific test wrapper classes
    - Update TestAll.java to remove deleted test references
    - Update TestMultithread.java to remove references to deleted tests
    - Update TestMultithreadFIPS.java to remove references to deleted tests
    
    Signed-off-by: Mohit Rajbhar <mohit.rajbhar@ibm.com>

--- src/test/java/ibm/jceplus/junit/TestMultithreadFIPS.java
+++ src/test/java/ibm/jceplus/junit/TestMultithreadFIPS.java
@@ -50,9 +50,6 @@ public class TestMultithreadFIPS {
             "ibm.jceplus.junit.openjceplusfips.multithread.TestRSAPSSInterop3",
             "ibm.jceplus.junit.openjceplusfips.multithread.TestRSASignatureInteropSunRsaSign",
             "ibm.jceplus.junit.openjceplusfips.multithread.TestSHA256Clone_SharedMD",
-            "ibm.jceplus.junit.openjceplusfips.multithread.TestSHA512",
-            "ibm.jceplus.junit.openjceplusfips.multithread.TestSHA512_224",
-            "ibm.jceplus.junit.openjceplusfips.multithread.TestSHA512_256",
             //"ibm.jceplus.junit.openjceplusfips.multithread.TestAESGCMWithKeyAndIvCheck", // test not in other test suites?
     };
     public final Object ob = new Object();
