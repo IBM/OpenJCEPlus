b9c59064223c4745d2362d07001f03b659beaa48
Tao Liu <tao.liu@ibm.com>
2026-09-11 20:36:41 -0400

    Pre-load sun.security.util classes at provider init
    Pre-load sun.security.util classes at provider init to fix
    SecurityManager access denial.
    
    SecurityManager.checkPackageAccess fires on first class load
    and walks every frame on the stack. For example, When
    ECParameters.internalInit() first loaded NamedCurve/CurveDB
    during an SSL handshake, an unprivileged jtreg test class was
    on the stack causing AccessControlException despite the
    openjceplus domain having the permission in default.policy.
    
    Pre-load all the affected sun.security.util classes under
    doPrivileged in the OpenJCEPlusProvider static initializer,
    before any Security Manager or unprivileged frame can exist.
    
    Signed-off-by: Tao Liu <tao.liu@ibm.com>

--- src/main/java/com/ibm/crypto/plus/provider/OpenJCEPlusProvider.java
+++ src/main/java/com/ibm/crypto/plus/provider/OpenJCEPlusProvider.java
@@ -56,7 +56,11 @@ public abstract class OpenJCEPlusProvider extends java.security.Provider {
     private AtomicInteger count = new AtomicInteger(0);
 
     @SuppressWarnings("exports")
-    protected static final Debug debug = Debug.getInstance(DEBUG_VALUE); 
+    protected static final Debug debug = Debug.getInstance(DEBUG_VALUE);
+
+    static {
+        SystemAccessUtils.preloadSunSecurityUtilClasses();
+    }
 
     OpenJCEPlusProvider(String name, String info) {
         super(name, PROVIDER_VER, info);
